4457d05d943c1510995a74fcce90a934dea9aeee
Jon Heise <j.heise@gmail.com>
2016-08-23 23:13:32 -0700

    moved base classes to seperate package

--- reporters/print_reporter.py
+++ reporters/print_reporter.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
 import argparse
-import analyzer
+import datastream.reporter
 
-class PrintAnalyzer(analyzer.Analyzer):
+class PrintReporter(datastream.reporter.Reporter):
 
-    def _analyze(self, data):
+    def _report(self, data):
         print data
-        return None
 
 def main(zmq_host, zmq_topic):
-    printer = PrintAnalyzer(zmq_host, zmq_topic, "9999")
+    printer = PrintReporter(zmq_host, zmq_topic)
     printer.activate()
     running = True
 
@@ -24,7 +23,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("Consume json from zmq and display data")
     parser.add_argument("-z", "--zmq", default="tcp://localhost:7777", help="ZMQ connection, default tcp://localhost:7777")
     parser.add_argument("-t", "--topic", default="dns", help="ZMQ topic, default dns")
-
     args = parser.parse_args()
 
     main(args.zmq, args.topic)
